fe2f5af60db39bdf538f3beb434287fb224b1ec2
김준하 <kimjoonha0426@gmail.com>
2026-01-07 23:25:59 +0900

    feat: init

---
+++ src/main/java/com/mycom/myapp/sendapp/SendappApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp.sendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycom/myapp/sendapp/SendappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp.sendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
