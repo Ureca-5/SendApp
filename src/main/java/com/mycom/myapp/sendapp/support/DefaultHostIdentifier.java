ced1c4f08b1cae1fe800c49d497c3bf152fed4e2
김준하 <kimjoonha0426@gmail.com>
2026-01-16 17:06:37 +0900

    feat(batch): 서버 호스트 식별자 생성 모듈

---
+++ src/main/java/com/mycom/myapp/sendapp/support/DefaultHostIdentifier.java
@@ -0,0 +1,45 @@
+package com.mycom.myapp.sendapp.support;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+
+/**
+ * 운영환경용 기본 구현체.
+ *
+ * - hostname:pid 형태로 반환합니다.
+ * - hostname 조회 실패 시 unknown-host로 대체합니다.
+ * - pid 조회 실패 시 unknown-pid로 대체합니다.
+ */
+@Component
+public class DefaultHostIdentifier implements HostIdentifier {
+    @Override
+    public String get() {
+        String host = resolveHostName();
+        String pid = resolvePid();
+        return host + ":" + pid;
+    }
+    private String resolveHostName() {
+        try {
+            // 일반적으로 mac/linux/container에서 hostname을 얻는 가장 무난한 방식
+            return InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            return "unknown-host";
+        }
+    }
+
+    private String resolvePid() {
+        try {
+            // "pid@hostname" 형태로 나오는 경우가 많음
+            String name = ManagementFactory.getRuntimeMXBean().getName();
+            int at = name.indexOf('@');
+            if (at > 0) {
+                return name.substring(0, at);
+            }
+            return name;
+        } catch (Exception e) {
+            return "unknown-pid";
+        }
+    }
+}
---
+++ src/main/java/com/mycom/myapp/sendapp/support/HostIdentifier.java
@@ -0,0 +1,16 @@
+package com.mycom.myapp.sendapp.support;
+
+/**
+ * 배치 실행 주체(호스트/프로세스/인스턴스)를 식별하기 위한 추상화.
+ *
+ * 개발 근거:
+ * - attempt.host_name 같은 컬럼에 값을 넣을 때, 호출부가 OS/컨테이너 환경에 직접 의존하지 않게 하기 위함입니다.
+ * - 추후 다중 서버/락 테이블 도입 시 locked_by 값으로 재사용 가능합니다.
+ */
+public interface HostIdentifier {
+    /**
+     * 사람/운영자가 로그/DB에서 봤을 때 유의미한 식별자 문자열을 반환합니다.
+     * 예: hostname:pid
+     */
+    String get();
+}
