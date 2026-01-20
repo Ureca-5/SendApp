9983c2e29ca9bd556833cfabe99b8d3300529e54
김준하 <kimjoonha0426@gmail.com>
2026-01-20 15:01:39 +0900

    feat(batch): 구독 원천 데이터 조회 모듈

---
+++ src/main/java/com/mycom/myapp/sendapp/batch/dto/SubscribeBillingHistoryRowDto.java
@@ -0,0 +1,23 @@
+package com.mycom.myapp.sendapp.batch.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class SubscribeBillingHistoryRowDto {
+    private Long subscribeBillingHistoryId;
+    private Long usersId;
+    private Long deviceId;
+    private Integer subscribeServiceId;
+    private String serviceName;
+    private LocalDate subscriptionStartDate;
+    private Long originAmount;
+    private Long discountAmount;
+    private Long totalAmount;
+    private Integer billingYyyymm;
+}
