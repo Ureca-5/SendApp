e4481cbb694962bc2128edc0e0106e9777559fbd
김준하 <kimjoonha0426@gmail.com>
2026-01-20 14:56:16 +0900

    feat(batch): MonthlyInvoiceHeaderPersisterImpl

---
+++ src/main/java/com/mycom/myapp/sendapp/batch/service/MonthlyInvoiceHeaderPersisterImpl.java
@@ -0,0 +1,29 @@
+package com.mycom.myapp.sendapp.batch.service;
+
+import com.mycom.myapp.sendapp.batch.dto.MonthlyInvoiceRowDto;
+import com.mycom.myapp.sendapp.batch.repository.invoice.MonthlyInvoiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Repository 호출
+ * 청구서 헤더 일괄 insert, 일괄 insert한 청구서 헤더의 usersId와 invoiceId 조회를 담당
+ */
+@RequiredArgsConstructor
+@Service
+public class MonthlyInvoiceHeaderPersisterImpl implements MonthlyInvoiceHeaderPersister {
+    private final MonthlyInvoiceRepository monthlyInvoiceRepository;
+
+    @Override
+    public void batchInsertHeaders(List<MonthlyInvoiceRowDto> headers) {
+        monthlyInvoiceRepository.batchInsert(headers);
+    }
+
+    @Override
+    public Map<Long, Long> findInvoiceIdsByUsers(Integer targetYyyymm, List<Long> usersIds) {
+        return monthlyInvoiceRepository.findInvoiceIdsByUsers(targetYyyymm, usersIds);
+    }
+}
