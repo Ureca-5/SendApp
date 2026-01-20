326c030bbcba4bb3ccc9fca8600b5461da98f496
김준하 <kimjoonha0426@gmail.com>
2026-01-20 20:46:45 +0900

    feat(batch): 청구서 헤더 repository 로직 소폭 수정

--- src/main/java/com/mycom/myapp/sendapp/batch/repository/invoice/MonthlyInvoiceRepository.java
+++ src/main/java/com/mycom/myapp/sendapp/batch/repository/invoice/MonthlyInvoiceRepository.java
@@ -6,7 +6,24 @@ import java.util.List;
 import java.util.Map;
 
 public interface MonthlyInvoiceRepository {
-    void batchInsert(List<MonthlyInvoiceRowDto> headers);
+    /**
+     * 청크 단위 헤더 신규 insert (batch) <br>
+     * - UK(users_id, billing_yyyymm) 충돌 시 DuplicateKeyException 발생 가능 <br>
+     * - "청구서 없는 유저만 Reader에서 뽑는다" 전제면 충돌은 드뭅니다. <br>
+     * - 각 헤더의 insert 결과(영향받은 레코드 수) 배열 반환
+     */
+    int[] batchInsert(List<MonthlyInvoiceRowDto> headers);
 
-    Map<Long, Long> findInvoiceIdsByUsers(Integer targetYyyymm, List<Long> usersIds);
+    /**
+     * 방금 insert된 헤더의 (users_id, invoice_id)를 일괄 조회합니다. <br>
+     * - where users_id in (...) and billing_yyyymm = ? <br>
+     * - 반환 row는 usersId, invoiceId만 채워서 반환
+     */
+    List<MonthlyInvoiceRowDto> findIdsByUsersIdsAndYyyymm(List<Long> usersIds, Integer billingYyyymm);
+
+    /**
+     * 청크 처리 후 totals / settlementSuccess / updatedAt 등을 일괄 update 합니다.
+     * - invoice_id 기준 batch update
+     */
+    int[] batchUpdateTotals(List<MonthlyInvoiceRowDto> headers);
 }
