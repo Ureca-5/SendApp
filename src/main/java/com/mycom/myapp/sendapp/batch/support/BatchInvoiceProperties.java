fcd0f91be5b759380580371e401557818d03bdd2
김준하 <kimjoonha0426@gmail.com>
2026-01-26 11:57:09 +0900

    feat(batch): 정산 배치 실패율 가중치 값 환경변수에 추가

--- src/main/java/com/mycom/myapp/sendapp/batch/support/BatchInvoiceProperties.java
+++ src/main/java/com/mycom/myapp/sendapp/batch/support/BatchInvoiceProperties.java
@@ -15,4 +15,5 @@ public class BatchInvoiceProperties {
     private final int microPageSize;
     private final int readerPageSize; // reader에서 한 번에 읽는 레코드 수
     private final int subDetailBatchSize; // 구독 상세 insert 레코드 수 단위
+    private final int failureWeight; // 정산 배치 실패 가중치(0 이하의 값이라면 가중치가 존재하지 않는 것으로 간주)
 }
